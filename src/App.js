6769f6f4c13011ce4c00d23cbcec0822c2b01366
Abhishek Dhapare <abhishek.dhapare@gmail.com>
2023-04-06 20:59:56 +0530

    initial setup of project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Tours from "./components/Tours";
+import Loading from "./components/Loading";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const allTours = tours.filter((tour) => tour.id !== id);
+    setTours(allTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    const response = await fetch(url);
+    const tours = await response.json();
+    // console.log(tours);
+    setTours(tours);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tour available</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
